f9b754358fb44caa20ab7ef73c9581ba7cad5c21
chmu7568 <christian.mueller@esolutions.de>
2025-09-11 16:37:22 +0200

    change hide score in top right corner when lose/win game

--- game/gui/score.gd
+++ game/gui/score.gd
@@ -7,8 +7,16 @@ extends Control
 var _label_score: int = 0
 
 
+func _ready():
+	Events.level_lose.connect(_on_level_lose)
+
+
 func _process(_delta):
 	visible = main.score > 0
 	if main.score > _label_score:
 		_label_score += 5
 	label.text = str(_label_score)
+
+
+func _on_level_lose():
+	hide()
